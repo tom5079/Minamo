ec472782702cc7c04ec0389a1d9bb627c9457d89
tom5079 <7948651+tom5079@users.noreply.github.com>
2023-08-16 19:47:22 -0700

    fix library build script

--- native/src/ssi.c
+++ native/src/ssi.c
@@ -1,9 +1,20 @@
 #include "ssi.h"
 
-int ssi_test() {
+int ssi_test(const char *file) {
+    VipsImage *in;
+    int width;
+
     if (vips_init("ssi_test")) {
         vips_error_exit(NULL);
     }
 
-    return 0;
+    if (!(in = vips_image_new_from_file(file, NULL))) {
+        vips_error_exit(NULL);
+    }
+
+    width = vips_image_get_width(in);
+
+    g_object_unref(in);
+
+    return width;
 }
--- native/test/test.c
+++ native/test/test.c
@@ -1,7 +1,18 @@
 #include <ssi.h>
 #include <stdio.h>
 
-int main() {
-    ssi_test();
+int main(int argc, char **argv) {
+    int width;
+
+    if (argc != 2) {
+        return 1;
+    }
+
+    width = ssi_test(argv[1]);
+
+    if (width != 2304) {
+        return 1;
+    }
+
     return 0;
 }
